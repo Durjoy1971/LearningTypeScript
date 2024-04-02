b49ae17693ffcc71f957738a04844bd8f09c4f80
Durjoy1971 <durjoybarua1971@gmail.com>
2024-04-02 20:01:06 +0600

    Part-01

---
+++ Intro.ts
@@ -0,0 +1,14 @@
+// Part - 01
+
+let num: number = 5;
+let s: string = "Durjoy";
+let flag: boolean = true;
+
+let direct_number = 10;
+let direct_string = "Barua";
+let direct_flag = false;
+
+console.log(num, s, flag);
+console.log(direct_number, direct_string, direct_flag);
+
+//
